644716ccd422eb2b2f29cfcd8d7775cc7ad2c3f5
graykode <nlkey2022@gmail.com>
2019-04-03 00:40:32 +0900

    add heketi.sh

---
+++ heketi.sh
@@ -0,0 +1,11 @@
+HEKETI_BIN="heketi-cli"      # heketi or heketi-cli
+HEKETI_VERSION="8.0.0"       # latest heketi version => https://github.com/heketi/heketi/releases
+HEKETI_OS="linux"            # linux or darwin
+
+curl -SL https://github.com/heketi/heketi/releases/download/v${HEKETI_VERSION}/heketi-v${HEKETI_VERSION}.${HEKETI_OS}.amd64.tar.gz -o /tmp/heketi-v${HEKETI_VERSION}.${HEKETI_OS}.amd64.tar.gz && \
+tar xzvf /tmp/heketi-v${HEKETI_VERSION}.${HEKETI_OS}.amd64.tar.gz -C /tmp && \
+rm -vf /tmp/heketi-v${HEKETI_VERSION}.${HEKETI_OS}.amd64.tar.gz && \
+cp /tmp/heketi/${HEKETI_BIN} /usr/local/bin/${HEKETI_BIN}_${HEKETI_VERSION} && \
+rm -vrf /tmp/heketi && \
+cd /usr/local/bin && \
+ln -vsnf ${HEKETI_BIN}_${HEKETI_VERSION} ${HEKETI_BIN} && cd
